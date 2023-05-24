d6bf3f61e349e610a6444ad6f90482405982c1ab
Bencze Réka <benczereka9@gmail.com>
2023-05-24 21:37:00 +0200

    add wait utility class

---
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/util/WaitClass.java
@@ -0,0 +1,15 @@
+package com.codecool.swaglabstestautomationprojectbrreka.util;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class WaitClass {
+    public static WebElement waitUntilClickable(WebDriver driver, WebElement element) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+
+
+}
