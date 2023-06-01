50afdb54fb9da64d812103fea3dcc94c9d0e1413
Bencze Réka <benczereka9@gmail.com>
2023-06-01 15:44:28 +0200

    add inventory page with elements and basic action

---
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/InventoryPage.java
@@ -0,0 +1,102 @@
+package com.codecool.swaglabstestautomationprojectbrreka.pages;
+
+import com.codecool.swaglabstestautomationprojectbrreka.util.WebdriverClass;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+import java.util.*;
+
+public class InventoryPage {
+
+    @FindBy(xpath = "//div[@class='app_logo']")
+    private WebElement pageTitle;
+
+    @FindBy(xpath = "//a[@class='shopping_cart_link']")
+    private WebElement shoppingCartLink;
+
+
+    //item links
+    @FindBy(xpath = "//div[@text='Sauce Labs Backpack']")
+    private WebElement slBackPackLink;
+
+    @FindBy(xpath = "//div[@text='Sauce Labs Bike Light']")
+    private WebElement slBikeLightLink;
+
+    @FindBy(xpath = "//div[@text='Sauce Labs Bolt T-Shirt']")
+    private WebElement slBoltTShirtLink;
+
+    @FindBy(xpath = "//div[@text='Sauce Labs Fleece Jacket']")
+    private WebElement slFleeceJacketLink;
+
+    @FindBy(xpath = "//div[@text='Sauce Labs Onesie']")
+    private WebElement slOnesieLink;
+
+    @FindBy(xpath = "//div[@text='Test.allTheThings() T-Shirt (Red)']")
+    private WebElement slAllTheThingsTShirtLink;
+
+    //add to cart button per item
+    @FindBy(xpath = "//button[@id='add-to-cart-sauce-labs-backpack']")
+    private WebElement addBackPackToCartButton;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-sauce-labs-bike-light']")
+    private WebElement addBikeLightToCartButton;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']")
+    private WebElement addBoltTShirtToCartButton;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-sauce-labs-fleece-jacket']")
+    private WebElement addFleeceJacketToCartButton;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-sauce-labs-onesie']")
+    private WebElement addOnesieToCartButton;
+
+    @FindBy(xpath = "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
+    private WebElement addAllTheThingsTShirtToCartButton;
+
+
+    public InventoryPage() {
+        WebDriver driver = WebdriverClass.getInstance();
+        PageFactory.initElements(driver,this);
+    }
+
+    public String getPageTitleText() {
+        return pageTitle.getText();
+    }
+
+    public void clickShoppingCart() {
+        shoppingCartLink.click();
+    }
+
+
+    //add things to cart separately
+    public void addBackpackToCart() {
+        addBackPackToCartButton.click();
+    }
+
+    public void addBikeLightToCart() {
+        addBikeLightToCartButton.click();
+    }
+
+    public void addBoltTShirtToCart() {
+        addBoltTShirtToCartButton.click();
+    }
+
+    public void addFleeceJacketToCart() {
+        addFleeceJacketToCartButton.click();
+    }
+
+    public void addOnesieToCart() {
+        addOnesieToCartButton.click();
+    }
+
+    public void addAllTheThingsTShirtToCart() {
+        addAllTheThingsTShirtToCartButton.click();
+    }
+
+    //add one item to cart
+    public void addItemToCartAndClickPay() {
+        addBackpackToCart();
+        clickShoppingCart();
+    }
+
+}
