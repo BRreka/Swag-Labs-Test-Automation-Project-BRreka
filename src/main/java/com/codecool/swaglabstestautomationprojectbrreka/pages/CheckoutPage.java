577f5e5aeef26e306466e80fb8aecdcfad3ce4ed
Bencze Réka <benczereka9@gmail.com>
2023-06-03 15:37:21 +0200

    add elements

--- src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutPage.java
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutPage.java
@@ -1,4 +1,28 @@
 package com.codecool.swaglabstestautomationprojectbrreka.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
 public class CheckoutPage {
+
+    @FindBy(xpath = "//span[text()='Checkout: Your Information']")
+    private WebElement checkoutPageTitle;
+
+    @FindBy(xpath = "//button[@id='cancel']")
+    private WebElement cancelBtn;
+
+    @FindBy(xpath = "//input[@id='continue']")
+    private WebElement continueBtn;
+
+    @FindBy(xpath = "//input[@id='first-name']")
+    private WebElement firstNameField;
+
+    @FindBy(xpath = "//input[@id='last-name']")
+    private WebElement lastNameField;
+
+    @FindBy(xpath = "//input[@id='postal-code']")
+    private WebElement zipCodeField;
+
+    @FindBy(xpath = "//h3[text()='Error: First Name is required']")
+    private WebElement errorMessage;
 }
