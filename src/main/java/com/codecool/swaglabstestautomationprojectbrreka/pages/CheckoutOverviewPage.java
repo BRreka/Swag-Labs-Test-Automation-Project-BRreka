ea9111d83ac57305611c148066e1bf8c0ce84499
Bencze Réka <benczereka9@gmail.com>
2023-06-03 18:28:17 +0200

    add elements

---
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutOverviewPage.java
@@ -0,0 +1,22 @@
+package com.codecool.swaglabstestautomationprojectbrreka.pages;
+
+import com.codecool.swaglabstestautomationprojectbrreka.util.WebdriverClass;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class CheckoutOverviewPage {
+
+    @FindBy(xpath = "//span[text()='Checkout: Overview']")
+    private WebElement checkoutOverviewPageTitle;
+
+    @FindBy(xpath = "div[@class='inventory_item_name']")
+    private WebElement inventoryItemName;
+
+    @FindBy(xpath = "//button[@id='cancel']")
+    private WebElement cancelBtn;
+
+    @FindBy(xpath = "//button[@id='finish']")
+    private WebElement finishBtn;
+
+
+}
