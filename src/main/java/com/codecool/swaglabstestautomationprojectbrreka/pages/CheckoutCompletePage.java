f8629d71696961f3e487ac2d97207cc73d17fce8
Bencze Réka <benczereka9@gmail.com>
2023-06-03 22:14:39 +0200

    add elements to checkout complete page

--- src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutCompletePage.java
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutCompletePage.java
@@ -6,6 +6,18 @@ import org.openqa.selenium.support.*;
 
 public class CheckoutCompletePage {
 
+    @FindBy(xpath = "//span[text()='Checkout: Complete!']")
+    private WebElement checkoutCompletePageTitle;
+
+    @FindBy(xpath = "//button[@id='back-to-products']")
+    private WebElement backHomeBtn;
+
+    @FindBy(xpath = "//h2[@class='complete-header']")
+    private WebElement completeHeader;
+
+    @FindBy(xpath = "//div[@class='complete-text']")
+    private WebElement completeText;
+
     public CheckoutCompletePage() {
         WebDriver driver = WebdriverClass.getInstance();
         PageFactory.initElements(driver,this);
