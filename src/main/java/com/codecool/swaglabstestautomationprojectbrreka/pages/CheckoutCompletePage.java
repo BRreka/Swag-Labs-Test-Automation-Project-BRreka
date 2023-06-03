00fcb42ab97f57ceac2f431fb12f72882519b123
Bencze Réka <benczereka9@gmail.com>
2023-06-03 22:05:13 +0200

    add constructor to checkout complete page

--- src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutCompletePage.java
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/pages/CheckoutCompletePage.java
@@ -1,4 +1,13 @@
 package com.codecool.swaglabstestautomationprojectbrreka.pages;
 
+import com.codecool.swaglabstestautomationprojectbrreka.util.WebdriverClass;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
 public class CheckoutCompletePage {
+
+    public CheckoutCompletePage() {
+        WebDriver driver = WebdriverClass.getInstance();
+        PageFactory.initElements(driver,this);
+    }
 }
