dda3f1defb35dc733eb7e0da291911fe2d66e068
Bencze Réka <benczereka9@gmail.com>
2023-06-14 21:32:18 +0200

    remove unnecessary class instances from login test

--- src/test/java/com/codecool/swaglabstestautomationprojectbrreka/LoginTest.java
+++ src/test/java/com/codecool/swaglabstestautomationprojectbrreka/LoginTest.java
@@ -16,10 +16,6 @@ import java.util.stream.*;
 public class LoginTest {
     static LoginPage mainPage;
     static InventoryPage inventoryPage;
-    static CartPage cartPage;
-    static CheckoutInfoPage checkoutInfoPage;
-    static CheckoutCompletePage checkoutCompletePage;
-    static CheckoutOverviewPage checkoutOverviewPage;
     static Properties properties;
 
     public static Stream<Arguments> provideStringsForLoginWith3DifferentKindOfData() {
@@ -42,10 +38,6 @@ public class LoginTest {
     public static void setUpAll() {
         mainPage = new LoginPage();
         inventoryPage = new InventoryPage();
-        cartPage = new CartPage();
-        checkoutInfoPage = new CheckoutInfoPage();
-        checkoutCompletePage = new CheckoutCompletePage();
-        checkoutOverviewPage = new CheckoutOverviewPage();
         properties = InitPropertiesClass.getInstance();
     }
 
