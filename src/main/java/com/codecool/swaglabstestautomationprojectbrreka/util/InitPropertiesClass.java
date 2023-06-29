51dfe233fe2bb414aa340868540bcea5a616a951
Zsofia Limbek <zsofia.limbek@gmail.com>
2023-06-29 16:33:14 +0200

    add constant for config path

--- src/main/java/com/codecool/swaglabstestautomationprojectbrreka/util/InitPropertiesClass.java
+++ src/main/java/com/codecool/swaglabstestautomationprojectbrreka/util/InitPropertiesClass.java
@@ -4,23 +4,23 @@ import java.io.*;
 import java.util.*;
 
 public class InitPropertiesClass {
+    private static final CONFIG_PROPERTIES_PATH;
     private static volatile Properties properties;
 
     private InitPropertiesClass() {}
 
-    public static Properties getInstance() {
+    public static Properties getInstance() throws IOException {
+        if (CONFIG_PROPERTIES_PATH == null) {
+            throw new IllegalArgumentException("Please provide config path");
+        }
         Properties prop = properties;
         if(prop == null) {
             synchronized (InitPropertiesClass.class) {
                 prop = properties;
                 if(prop == null) {
                     prop = new Properties();
-                    try {
-                        FileInputStream ip = new FileInputStream("config.properties path");
-                        prop.load(ip);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    FileInputStream ip = new FileInputStream(CONFIG_PROPERTIES_PATH);
+                    prop.load(ip);
                 }
             }
         }
